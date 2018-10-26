b02279f0ffa66e975e007b93f63c0475e47f1862
Clinton Johnson <dragoon1246224@gmail.com>
2018-10-26 12:46:37 -0400

    Done.

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ app.use(express.json())
 
 // Fill in your request handlers here
 
+app.post("/updates", function (req, res){
+    if(req.body.clientUpdates.length > 0) {
+        req.body.clientUpdates.forEach(update => updates.push(update))
+    }
+    newUpdates = updates.slice(req.body.lengthOfLastServerUpdate)
+    res.send({
+        "serverUpdates": newUpdates
+    });
+})
+
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,29 @@
 // Add logic to this script to poll server every second for updated pixels.
+const timeoutLength = 1000;
+let lengthOfLastServerUpdate = 0;
+
+const pollServer = async function () {
+    let body = {
+        "clientUpdates": clientUpdates,
+        "lengthOfLastServerUpdate": lengthOfLastServerUpdate
+    }
+    body = JSON.stringify(body);
+    clientUpdates = [];
+    let res = await fetch('/updates', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: body
+    })
+    res = await res.json();
+    lengthOfLastServerUpdate += res.serverUpdates.length
+    if (res.serverUpdates.length > 0) {
+        res.serverUpdates.forEach(update => {
+            bitmap.applyUpdatesFromServer(update[0],update[1],update[2])
+        })
+    }
+
+    setTimeout(pollServer, timeoutLength);
+};
+setTimeout(pollServer, timeoutLength);
